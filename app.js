de5c4d66761b213105bd206e825cd80897830b2f
ajaymeru <123156354+ajaymeru@users.noreply.github.com>
2024-05-30 14:28:10 +0530

    navbar

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 
 function toggleNav() {
     const contact = document.querySelector('.navbar-TM');
-    contact.classList.toggle('open');
     contact.classList.toggle('hidden');
+    contact.classList.toggle('open');
+    
+    const icon = document.querySelector('.menu-icon');
+    if (icon.getAttribute('data-icon') === 'hamburger') {
+        icon.innerHTML = "&#9776;";
+        icon.setAttribute('data-icon', 'close');
+    } else {
+        icon.innerHTML = "X";
+        icon.setAttribute('data-icon', 'hamburger');
+    }
 }
